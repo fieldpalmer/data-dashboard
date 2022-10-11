66be6a2997dc74961c7229a9dc7938364b221127
Field Palmer <gfieldpalmer@gmail.com>
2022-10-11 01:41:17 -0600

    charts and graphs pages working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,33 @@ import avatar from '../data/avatar.png';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
-const NavButton = ({title, customFunc, icon, color, dotColor}) => (
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent content={title} position="BottomCenter">
-    <button type="button" onClick={customFunc} style={{ color }} className="relative text-sl rounded-full p-3 hover:bg-light-gray">
-      <span style={{ background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2" />
-        {icon}
+    <button
+      type="button"
+      onClick={() => customFunc()}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+      />
+      {icon}
     </button>
   </TooltipComponent>
-)
+);
 
 const Navbar = () => {
   const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
+
     window.addEventListener('resize', handleResize);
+
     handleResize();
+
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
@@ -39,7 +50,8 @@ const Navbar = () => {
   const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className="flex justify-between p-2 md:mx-6 relative">
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
+
       <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
       <div className="flex">
         <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
@@ -71,7 +83,7 @@ const Navbar = () => {
         {isClicked.userProfile && (<UserProfile />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
